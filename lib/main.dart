a5c1858447210f4ac2550b750e484842510f4956
murilomanfre <aruak@outlook.com>
2025-04-07 22:24:58 -0300

    inicializando aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flow/providers/catalog_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'app_widget.dart';
@@ -5,9 +6,10 @@ import 'providers/auth_provider.dart';
 
 void main() {
   runApp(
-    MultiProvider(
-      providers: [
+    MultiProvider(providers
+      : [
         ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (_) => CatalogProvider()),
       ],
       child: const AppWidget(),
     ),
